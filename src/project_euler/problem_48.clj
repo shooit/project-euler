fe79810e82fb4b62b9f9e88db1c6bbff18e7786a
Sam Hewitt <shewitt@ccri.com>
2016-06-14 00:46:17 -0400

    documentation for 48

--- src/project_euler/problem_48.clj
+++ src/project_euler/problem_48.clj
@@ -4,10 +4,12 @@
 
 
 (defn self-power
+  "Returns the value of [n] to the power of [n]"
   [n]
   (math/expt n n))
 
 (defn sum-self-powers
+  "Sums all the self powers up to [max]"
   [max]
   (apply + (for [n (range 1 (+ max 1))] (self-power n))))
 
